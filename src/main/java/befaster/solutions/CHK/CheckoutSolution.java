a6e24a77e5b2fe11dc2436834deb7b08af88c72a
sbx_user1051 <sbx_user1051@169.254.52.221>
2024-06-14 11:07:32 +0000

    Fri Jun 14 11:07:32 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,28 +3,62 @@ package befaster.solutions.CHK;
 import befaster.runner.SolutionNotImplementedException;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CheckoutSolution {
-    public Integer checkout(String skus) {
-//        private static final Map<Character, Integer> prices;
-//        private static final Map<Character, int[]> oferteSpeciale;
+    private static final Map<Character, Integer> prices;
+    private static final Map<Character, int[]> oferteSpeciale;
 
-//        static {
-//            prices = new HashMap<>();
-//            prices
-//        }
-        Integer total = 0;
+    static {
+        prices = new HashMap<>();
+        prices.put('A', 50);
+        prices.put('B', 30);
+        prices.put('C', 20);
+        prices.put('D', 15);
+
+        oferteSpeciale = new HashMap<>();
+        oferteSpeciale.put('A', new int[]{3, 130});
+        oferteSpeciale.put('B', new int[]{2, 45});
+    }
+    public Integer checkout(String skus) {
         if (skus == null) {
             return -1;
         }
+        Map<Character, Integer> cerere = new HashMap<>();
 
-        return -1;
+        for (char c :skus.toCharArray()) {
+            if (!prices.containsKey(c)) {
+                return -1;
+            }
+            cerere.put(c, cerere.getOrDefault(c, 0) + 1);
+
+        }
+        Integer total = 0;
+        for (Map.Entry<Character, Integer> e : cerere.entrySet()) {
+            char c = e.getKey();
+            int count = e.getValue();
+            int price = prices.get(c);
+
+            if (oferteSpeciale.containsKey(c)) {
+                int[] offer = oferteSpeciale.get(c);
+                int offerCount = offer[0];
+                int offerPrice = offer[1];
+
+                int numOffers = count/offerCount;
+                int remain = count % offerCount;
+
+                total += numOffers * offerPrice + remain * price;
+            } else {
+                total += count * price;
+            }
+        }
+        return total;
     }
 
     public static void main(String[] args) {
         CheckoutSolution solution = new CheckoutSolution();
         System.out.println(solution.checkout(args[0]));
-        System.out.println(args[0]);
     }
 }
+
 
